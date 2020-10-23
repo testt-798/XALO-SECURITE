e6175756948eeb5a1af4b7bcb73caa66d767d8dd
testt-798 <70701714+testt-798@users.noreply.github.com>
2020-10-23 21:02:21 +0300

    Add files via upload

---
+++ env.js
@@ -0,0 +1,14 @@
+# Environment Config
+
+# store your secrets and config variables in here
+# only invited collaborators will be able to see your .env values
+
+# reference these in your code with process.env.SECRET
+
+PREFIX=-
+BOT_TOKEN=NzI2NjU5OTM4NjkyMjM1MzQ0.XwNmfA.aKWQisLEzZiKpZkXyI5SSwIU95s
+YTAPI_KEY=AIzaSyAv9bQOZON0kF-OKmrKMQE40ulHfiPVD7s
+PROJECT_DOMAIN=mufobot
+
+
+# note: .env is a shell file so there can't be spaces around =
